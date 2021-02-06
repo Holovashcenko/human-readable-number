b5a2e4d96309a8d73745710c57c72ebdba495e60
Vladyslav Holovashchenko <holovashcenko@gmail.com>
2021-02-06 15:47:44 +0200

    fead: realized toReadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const strNum = {
+        0: "zero",
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine",
+        10: "ten",
+        11: "eleven",
+        12: "twelve",
+        13: "thirteen",
+        14: "fourteen",
+        15: "fifteen",
+        16: "sixteen",
+        17: "seventeen",
+        18: "eighteen",
+        19: "nineteen",
+        20: "twenty",
+        30: "thirty",
+        40: "forty",
+        50: "fifty",
+        60: "sixty",
+        70: "seventy",
+        80: "eighty",
+        90: "ninety",
+        100: "hundred",
+    };
+    if(number <= 20) return strNum[number]
+    if (String(number).length === 2 && Number.isInteger(number/10)) return strNum[number];
+    if (String(number).length === 2 && number < 100 && number > 20) return strNum[Math.floor(number/10) * 10] + " " + strNum[String(number)[1]];
+    if (String(number).length === 3 && Number.isInteger(number/100)) return strNum[String(number)[0]] + " " + strNum[100];
+    if (String(number).length === 3 && Number.isInteger(number/10)) return strNum[String(number)[0]] + " " + strNum[100] + " " + strNum[+String(number)[1] * 10];
+    if (String(number).length === 3 && number <= String(number)[0]+20) return strNum[String(number)[0]] + " " + strNum[100] + " " + strNum[+String(number).slice(1)];
+    if (String(number).length === 3 && number > String(number)[0]+20) return strNum[String(number)[0]] + " " + strNum[100] + " " + strNum[+String(number)[1] * 10] + " " + strNum[+String(number)[2]];
+};
